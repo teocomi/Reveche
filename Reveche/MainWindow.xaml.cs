200ac0def6e2574f627a70e5705e8113da046fab
Jonathon Broughton <jonathon@stardotbmp.com>
2019-02-22 17:04:06 +0000

    Remove prior attempt at clipboard handling

--- Reveche/MainWindow.xaml.cs
+++ Reveche/MainWindow.xaml.cs
@@ -336,7 +336,7 @@ namespace Reveche
 
    
 
-
+ 
 
     #region version stuff
 
@@ -512,43 +512,6 @@ namespace Reveche
 
     #endregion
 
-    }
-  }
-
-  public static class Clipboard {
-    [DllImport( "user32.dll" )]
-    static extern IntPtr GetClipboardData( uint uFormat );
-    [DllImport( "user32.dll" )]
-    static extern bool IsClipboardFormatAvailable( uint format );
-    [DllImport( "user32.dll", SetLastError = true )]
-    static extern bool OpenClipboard( IntPtr hWndNewOwner );
-    [DllImport( "user32.dll", SetLastError = true )]
-    static extern bool CloseClipboard();
-    [DllImport( "kernel32.dll" )]
-    static extern IntPtr GlobalLock( IntPtr hMem );
-    [DllImport( "kernel32.dll" )]
-    static extern bool GlobalUnlock( IntPtr hMem );
-
-    const uint CF_UNICODETEXT = 13;
-
-    public static string GetText() {
-      if ( !IsClipboardFormatAvailable( CF_UNICODETEXT ) )
-        return null;
-      if ( !OpenClipboard( IntPtr.Zero ) )
-        return null;
-
-      string data = null;
-      var hGlobal = GetClipboardData( CF_UNICODETEXT );
-      if ( hGlobal != IntPtr.Zero ) {
-        var lpwcstr = GlobalLock( hGlobal );
-        if ( lpwcstr != IntPtr.Zero ) {
-          data = Marshal.PtrToStringUni( lpwcstr );
-          GlobalUnlock( lpwcstr );
-        }
-      }
-      CloseClipboard();
-
-      return data;
     }
   }
 }
